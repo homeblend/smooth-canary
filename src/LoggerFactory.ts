66dd1cf0bc7c1d389f467b0feba405767a6677e8
willwsharp <willwsharp@gmail.com>
2018-12-02 19:08:58 -0500

    adding more logging and config

---
+++ src/LoggerFactory.ts
@@ -0,0 +1,24 @@
+import { TransformableInfo } from 'logform';
+import winston from 'winston';
+
+export default function createLogger() {
+    const logger = winston.createLogger({
+        level: 'info',
+        format: winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.printf((info: TransformableInfo) => {
+                return `${info.timestamp} ${info.level}: ${info.message}`;
+            })
+        ),
+        transports: [
+            new winston.transports.Console(),
+            new winston.transports.File({
+                // make this config driven
+                filename: '/var/log/smooth-canary/smooth-canary.log',
+                maxsize: 10000000, // 10 MB, make this config driven,
+                maxFiles: 20 // only allow max of 20 files
+            })
+        ],
+    });
+    return logger;
+}
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,43 @@
+import dotenv from 'dotenv';
 import express from 'express';
 import http from "http";
+import { AddressInfo } from 'net';
 import socketIO from "socket.io";
+import createLogger from './LoggerFactory';
+import { isDefined } from './utils';
+
+// loading environment file
+dotenv.config();
+
+const logger = createLogger();
 const app = express();
 const server = new http.Server(app);
+// setting defaults
+const defaultHostName = "http://localhost";
+const defaultPort = '8080';
+
 // initialize socketIO
 const socketIOServer = socketIO(server);
-server.listen(8080);
-
-console.log("smooth-canary started");
+if (!isDefined(process.env.HOSTNAME)) {
+	logger.info(`Server hostname was not specified in config file; using default of ${defaultHostName}`);
+	process.env.HOSTNAME = defaultHostName;
+}
+if (!isDefined(process.env.SERVER_PORT)) {
+	logger.info(`Server port was not specified in config file; using default of ${defaultPort}`);
+	process.env.SERVER_PORT = defaultPort;
+}
+server.listen(process.env.SERVER_PORT, defaultHostName);
+const address = server.address() as AddressInfo;
+const host = address.address;
+const port = address.port;
+logger.info(`smooth-canary server started on ${host}:${port}`);
 socketIOServer.on('connection', (socket: SocketIO.Socket) => {
-  console.log("Received connection");
-  socket.on('gas-volatility', (data) => {
-    console.log("Handling Gas Volatility Message");
-  });
+	logger.info("A new client has connected");
+	socket.on('gas-volatility', (data) => {
+		logger.info("Handling Gas Volatility Message");
+		logger.info(data);
+	});
+});
+socketIOServer.on('close', (socket: SocketIO.Socket) => {
+	logger.info("A client has disconnected");
 });
